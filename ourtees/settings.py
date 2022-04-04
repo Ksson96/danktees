4beb44f7cbd98cd1a7d7d443b390d7b7f1d17bc5
Anton Andersson <anton.andersson001@live.se>
2022-04-04 11:21:52 +0000

    Import Django Allauth Templates

--- ourtees/settings.py
+++ ourtees/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'https://8000-ksson96-ourtees-qzphv6bz6zj.ws-eu38.gitpod.io']
+
+CSRF_TRUSTED_ORIGINS = ['https://8000-ksson96-ourtees-qzphv6bz6zj.ws-eu38.gitpod.io']
 
 
 # Application definition
