c54d2461ebdd04d6725b707368da2277a27feaf7
Anton Andersson <anton.andersson001@live.se>
2022-04-08 15:25:00 +0000

    Recreate Products App, Products, Category and Size model

--- home/models.py
+++ home/models.py
@@ -1,7 +1,2 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-
-class Category(models.Model):
-    """Category Model"""
-    name = models.CharField(max_length=200)
--- products/models.py
+++ products/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -7,16 +6,16 @@ class Category(models.Model):
     name = models.CharField(max_length=200)
     category_type = models.CharField(max_length=200, blank=True, null=True)
 
-    # def __str__(self):
-    #     return self.title
+    def __str__(self):
+        return self.name
 
 
 class Size(models.Model):
     """Size Model"""
     size = models.CharField(max_length=3)
 
-    # def __str__(self):
-    #     return self.size
+    def __str__(self):
+        return self.size
 
 
 class Product(models.Model):
@@ -25,10 +24,7 @@ class Product(models.Model):
     name = models.CharField(max_length=250)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    size = models.ManyToManyField(Size, related_name='product_size', null=True, blank=True)
+    size = models.ManyToManyField(Size, related_name='product_size', blank=True)
     rating = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-
-    # def __str__(self):
-    #     return self.name
