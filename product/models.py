79293ad8f5ec352e34fd42c2c716b421f2a6aff6
Anton Andersson <anton.andersson001@live.se>
2022-04-08 14:57:54 +0000

    Create Products app and setup Product, Size and Category Models

---
+++ product/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    """Category Model"""
+    name = models.CharField(max_length=200)
+    category_type = models.CharField(max_length=200, blank=True, null=True)
+
+    # def __str__(self):
+    #     return self.title
+
+
+class Size(models.Model):
+    """Size Model"""
+    size = models.CharField(max_length=3)
+
+    # def __str__(self):
+    #     return self.size
+
+
+class Product(models.Model):
+    """Product Model"""
+    category = models.ForeignKey(Category, on_delete=models.SET_DEFAULT, default='', related_name='product_category')
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    size = models.ManyToManyField(Size, related_name='product_size', null=True, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    # def __str__(self):
+    #     return self.name
