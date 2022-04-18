af20790750b348a8443b5a3b7c8f725f1fe6dc2d
Anton Andersson <anton.andersson001@live.se>
2022-04-18 18:03:08 +0000

    Filter options for theme in product_list template

--- products/views.py
+++ products/views.py
@@ -1,22 +1,34 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Product, Category
+from django.db.models import Q
 
 
 def product_list(request):
     """ All products """
     products = Product.objects.all()
-    category = None
+    category = ""
     current_category = None
+    theme = ""
 
     if request.GET:
-        category = request.GET.get('category')
-        products = get_list_or_404(Product, category=category)
-        current_category = get_object_or_404(Category, category_type=category)
+
+        if 'category' in request.GET:
+            category = request.GET['category']
+            # products = get_list_or_404(Product, category=category)
+            current_category = get_object_or_404(Category, category_type=category)
+
+        if 'theme' in request.GET:
+            theme = request.GET.get('theme')
+            # products = products.filter(theme__theme__contains=theme)
+
+        query_filters = Q(theme__theme__contains=theme) & Q(category__category_type__contains=category)
+        products = products.filter(query_filters)
 
     context = {
         'products': products,
         'category': category,
         'current_category': current_category,
+        'theme': theme,
     }
 
     return render(request, 'products/product_list.html', context)
