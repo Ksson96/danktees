a2ddaba57a55cd4ab7ddac7f7431b9df95cb8e1c
Anton Andersson <anton.andersson001@live.se>
2022-04-22 15:19:56 +0000

    Add search and sorting functions to product list

--- products/views.py
+++ products/views.py
@@ -5,11 +5,14 @@ from django.db.models import Q
 
 def product_list(request, category_type):
     """ All products """
-    category = category_type
+    category = None
     theme = ""
+    search_query = None
+    q = None
 
     if category_type == 'all':
         products = Product.objects.all()
+        
     else:
         category = get_object_or_404(Category, category_type=category_type)
         products = category.product_category.all()
@@ -19,14 +22,21 @@ def product_list(request, category_type):
             theme = request.GET.get('theme')
             products = products.filter(theme__theme__contains=theme)
 
-        # query_filters = Q(theme__theme__contains=theme) & Q(category__category_type__contains=category)
-        # products = products.filter(query_filters)
+        if 'search' in request.GET:
+            search_query = request.GET['search']
+            query = Q(name__icontains=search_query) | Q(theme__theme__icontains=search_query)
+            products = Product.objects.filter(query)
+
+        if 'sortby' in request.GET:
+            sort_query = request.GET['sortby']
+            products = products.order_by(sort_query)
 
     context = {
         'products': products,
         'category': category,
         'theme': theme,
         'category_type': category_type,
+        'search_query': search_query,
     }
 
     return render(request, 'products/product_list.html', context)
